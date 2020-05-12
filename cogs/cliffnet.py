9a23697f4444ed033e0570bfdb41b9fa22f27f5f
KrankyK <64539260+KrankyK@users.noreply.github.com>
2020-05-12 11:12:47 +0300

    Update cliffnet.py
    Renamed input into userInput for safety.

--- cogs/cliffnet.py
+++ cogs/cliffnet.py
@@ -28,8 +28,8 @@ class cliffnet(commands.Cog):
         if ctx.guild.id == 107847342006226944:
             get_message_task = asyncio.create_task(ctx.channel.fetch_message(ctx.channel.last_message_id))
             await get_message_task
-            input = get_message_task.result()
-            split_message = input.split(' ')
+            userInput = get_message_task.result()
+            split_message = userInput.split(' ')
             if len(split_message) > 0:
                 result = random.sample(split_message, len(split_message))
                 stringResult= " "
@@ -47,11 +47,11 @@ class cliffnet(commands.Cog):
         if ctx.guild.id == 107847342006226944:
             await ctx.trigger_typing()
             try:
-                input = Helper.CommandStrip(ctx.message.content)
-                if input=="":
+                userInput = Helper.CommandStrip(ctx.message.content)
+                if userInput=="":
                     return await ctx.send(f'Enter a keyword to search')
                     
-                source = (f'http://newsapi.org/v2/everything?q={input}&sortBy=top&apiKey=fec0d23dd26549a9a6d58a29a675e764')
+                source = (f'http://newsapi.org/v2/everything?q={userInput}&sortBy=top&apiKey=fec0d23dd26549a9a6d58a29a675e764')
                 
                 pull = requests.get(source)
 
@@ -147,8 +147,8 @@ class cliffnet(commands.Cog):
                 logger.LogPrint(f'Error during initial file read / creation: {ex}',logging.ERROR)
         
             #return all entries
-            input = Helper.CommandStrip(ctx.message.content).upper()
-            if input == "": 
+            userInput = Helper.CommandStrip(ctx.message.content).upper()
+            if userInput == "": 
                 response = f">>> Days since last reset: "
                 for x in daysDict:
                     timeDeltaDif = datetime.datetime.utcnow() - daysDict[x][0]
@@ -156,49 +156,49 @@ class cliffnet(commands.Cog):
                 await ctx.send(response)
             
             #add new timer
-            elif input.split(' ')[0].upper() not in keyWords: 
+            elif userInput.split(' ')[0].upper() not in keyWords: 
                 
-                if input in daysDict:
-                    timeDeltaDif = datetime.datetime.utcnow() - daysDict[input][0]
-                    return await ctx.send(f">>> \"{input}\" - {timeDeltaFormat(timeDeltaDif)[0]} days and {timeDeltaFormat(timeDeltaDif)[1]} hours since last reset")
+                if userInput in daysDict:
+                    timeDeltaDif = datetime.datetime.utcnow() - daysDict[userInput][0]
+                    return await ctx.send(f">>> \"{userInput}\" - {timeDeltaFormat(timeDeltaDif)[0]} days and {timeDeltaFormat(timeDeltaDif)[1]} hours since last reset")
                 else:
-                    daysDict[input.upper()] = [datetime.datetime.utcnow(),0,0]
+                    daysDict[userInput.upper()] = [datetime.datetime.utcnow(),0,0]
                     with open(daysFile,"wb") as daysFileWriter:
                         pickle.dump(daysDict, daysFileWriter)
                         return await ctx.send(f">>> Successfully added to the list!")
             
             #reset an existing timer
-            elif input.startswith("ZERO"): 
+            elif userInput.startswith("ZERO"): 
 
                 pattern = "ZERO "
-                input = input.upper()
-                input = re.split(pattern, input, 1)[1]
-                if input in daysDict:
-                    lastDate=daysDict[input][0]
-                    recordLength=daysDict[input][2]
+                userInput = userInput.upper()
+                userInput = re.split(pattern, userInput, 1)[1]
+                if userInput in daysDict:
+                    lastDate=daysDict[userInput][0]
+                    recordLength=daysDict[userInput][2]
                     currentDate=datetime.datetime.utcnow()
                     lastLength=currentDate-lastDate
 
-                    if daysDict[input][2] == None or daysDict[input][2] == 0 or daysDict[input][2] < lastLength: 
+                    if daysDict[userInput][2] == None or daysDict[userInput][2] == 0 or daysDict[userInput][2] < lastLength: 
                         recordLength = lastLength
-                    daysDict[input]=[currentDate,lastLength,recordLength]
-                    await ctx.send(f">>> \"{input}\" lasted {timeDeltaFormat(lastLength)[0]} days and {timeDeltaFormat(lastLength)[1]} hours. Longest record {timeDeltaFormat(recordLength)[0]} days and {timeDeltaFormat(recordLength)[1]} hours.")
+                    daysDict[userInput]=[currentDate,lastLength,recordLength]
+                    await ctx.send(f">>> \"{userInput}\" lasted {timeDeltaFormat(lastLength)[0]} days and {timeDeltaFormat(lastLength)[1]} hours. Longest record {timeDeltaFormat(recordLength)[0]} days and {timeDeltaFormat(recordLength)[1]} hours.")
                     with open(daysFile,"wb") as daysFileWriter:
                         pickle.dump(daysDict, daysFileWriter)
                 else:
                     return await ctx.send(f">>> Entry for timer reset not found.")
 
-            elif input.startswith("DELETE"): #delete an existing timer
+            elif userInput.startswith("DELETE"): #delete an existing timer
                 pattern = "DELETE "
-                input = re.split(pattern, input, 1)[1]
+                userInput = re.split(pattern, userInput, 1)[1]
 
-                if input in daysDict:
-                    daysDict.pop(input)
+                if userInput in daysDict:
+                    daysDict.pop(userInput)
                     with open(daysFile,"wb") as daysFileWriter:
                         pickle.dump(daysDict, daysFileWriter)
-                        return await ctx.send(f">>> \"{input}\" Deleted from list")
+                        return await ctx.send(f">>> \"{userInput}\" Deleted from list")
                 else:
-                    return await ctx.send(f">>> \"{input}\" - was not found for deletion.")        
+                    return await ctx.send(f">>> \"{userInput}\" - was not found for deletion.")        
 
         except FileNotFoundError as ex:
             logger.LogPrint(f'ERROR - Days file not found - {ex}', logging.ERROR)
