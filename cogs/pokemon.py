fa419baabab74d7f9977094787f9d979c53c461c
NeighbourJim <c.proudmore@gmail.com>
2020-06-20 20:59:09 +0800

    Added !rname and !generatepokemon commands
    Generate Pokemon will generate an entirely new Pokemon.
    It will generate a name (from combining two existing pokemon names), a typing, a set of abilities, and a stat spread.
    
    !Rname will generate just a pokemon name, with the same method.

--- cogs/pokemon.py
+++ cogs/pokemon.py
@@ -136,7 +136,134 @@ class Pokemon(commands.Cog):
             
         else:
             await ctx.send(f'{ctx.message.author.mention}: No pokemon with that name or number found.')
+    
+    @commands.command(help="Generate a Pokemon name.", aliases=['rname', 'RName', 'Rname'])
+    @commands.cooldown(rate=1, per=2, type=BucketType.channel)
+    @commands.has_role("Bot Use")
+    @commands.guild_only()
+    async def randomname(self, ctx):
+        # Generate Name
+        name1 = random.choice(pokemon_names).lower()
+        name2 = random.choice(pokemon_names).lower()
 
+        roll = random.randint(0,3)
+        if roll == 0:
+            name = name1[:len(name1)//2] + name2[len(name2)//2:]
+        elif roll == 1:
+            name = name1[len(name1)//2:] + name2[:len(name2)//2]
+        elif roll == 2:
+            name = name2[len(name2)//2:] + name1[:len(name1)//2]
+        elif roll == 3:
+            name = name2[:len(name2)//2] + name1[len(name1)//2:]
+        name = name.title()
+
+        await ctx.send(f'{ctx.message.author.mention}: {name}')
+
+    @commands.command(help="Generate an entirely random new Pokemon.", aliases=['gpoke', 'genpoke', 'GPoke', 'Genpoke', 'gmon'])
+    @commands.cooldown(rate=1, per=5, type=BucketType.channel)
+    @commands.has_role("Bot Use")
+    @commands.guild_only()
+    async def generatepokemon(self, ctx):
+        types = ['normal','fire','water','grass','fighting','flying','poison','electric','ground','psychic','rock','ice','bug','dragon','ghost','dark','steel','fairy']
+        stats = []
+
+        await ctx.trigger_typing()
+
+        # Generate Name
+        name1 = random.choice(pokemon_names).lower()
+        while True:
+            name2 = random.choice(pokemon_names).lower()
+            if name1 != name2:
+                break
+
+        roll = random.randint(0,3)
+        if roll == 0:
+            name = name1[:len(name1)//2] + name2[len(name2)//2:]
+        elif roll == 1:
+            name = name1[len(name1)//2:] + name2[:len(name2)//2]
+        elif roll == 2:
+            name = name2[len(name2)//2:] + name1[:len(name1)//2]
+        elif roll == 3:
+            name = name2[:len(name2)//2] + name1[len(name1)//2:]
+        name = name.title()
+
+
+        # Generate Typing
+        if random.randint(0,100) <= 40:
+            # single typed
+            typing = random.choice(types).title()
+        else:
+            # dual typed
+            t1 = random.choice(types).title()
+            while True:
+                t2 = random.choice(types).title()
+                if t1 != t2:
+                    break
+            typing = f'{t1}/{t2}'
+
+        # Generate Stats 
+        stats = []
+        min_base_bst = 300
+        max_base_bst = 720
+        # get a random base total to use
+        base_bst = random.randint(min_base_bst,max_base_bst)
+        # decide the stat weight
+        # clamped float value between 0.8 and 1.0, varying based on the base total
+        # higher base totals will tend to have higher stat weight
+        # On average (over 10000 runs) this process should result in an average BST of about 460, min 180, max 725.
+        stat_weight = round(max(min((base_bst / max_base_bst) * 1.5, 1.0), 0.8), 2)
+        for i in range(6):
+            stat = random.randint(100,200) + random.randint(-50, 55)
+            if stat > 255:
+                stat = 255
+            if stat < 5:
+                stat = 5
+            # subtract the stat from the remaining stat total, multiplying by stat weight
+            # doing this should get more relatively accurate stat spreads ON AVERAGE (you can still end up with absolutely pathetic stats)
+            base_bst -= round(stat * stat_weight)
+            if base_bst < 50:
+                stat = random.randint(25,50)
+            stats.append(stat)
+        random.shuffle(stats)
+        stat_total = sum(stats)
+
+        # Generate Abilities
+        api_results = Helpers.GetWebPage(self, 'https://pokeapi.co/api/v2/ability/?offset=0&limit=233')
+        if api_results:
+            ability1 = random.choice(api_results.json()['results'])['name']
+            ability1 = ability1.title().replace("-", " ")
+            while True:
+                ability2 = random.choice(api_results.json()['results'])['name']
+                ability2 = ability2.title().replace("-", " ")
+
+                ability3 = random.choice(api_results.json()['results'])['name']
+                ability3 = ability3.title().replace("-", " ")
+
+                if ability1 != ability2 != ability3:
+                    break
+        else:
+            await ctx.send(f'{ctx.message.author.mention}: Something went wrong when contacting the API.')
+            return
+
+        poke_embed = discord.Embed()
+        poke_embed.title = f'{name}'
+        poke_embed.add_field(name='Type', value=typing, inline=True)
+        poke_embed.add_field(name='Abilities', value=f'{ability1}, {ability2}, {ability3} (Hidden)', inline=False)
+        poke_embed.add_field(name=f'BST: {stat_total}', value=(
+            f"**HP:** {stats[0]}\n"
+            f"**ATK:** {stats[1]}\n"
+            f"**DEF:** {stats[2]}\n"
+            ), inline=True)
+        poke_embed.add_field(name=f'Stats', value=(
+            f"**SP.ATK:** {stats[3]}\n"
+            f"**SP.DEF:** {stats[4]}\n"
+            f"**SPE:** {stats[5]}\n"
+        ), inline=True)
+        poke_embed.set_footer(text=f'Generated for {ctx.message.author.name}')
+        await ctx.send(embed=poke_embed)
+        
+
+        
 
 
 def setup(client):
