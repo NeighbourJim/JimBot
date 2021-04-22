182bc906f62644840ea6ec7be788b5899ed31661
NeighbourJim <c.proudmore@gmail.com>
2021-04-22 20:39:37 +0800

    Added Cleverbot functionality
    Currently hard coded to be limited to one personal server, but can easily be opened up.

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 import sys
+import random
 import os
 import logging
 import discord
+from cleverwrap import CleverWrap
 from discord.ext import commands
 
 
@@ -14,11 +16,17 @@ import internal.configmanager as configmanager
 # Start the logger, load the current settings, create the client
 logger.StartLogging()
 current_settings = configmanager.cm.GetConfig()
+cw = CleverWrap(f'{current_settings["keys"]["cleverbot_key"]}')
+convo = cw.new_conversation()
+hitroll = 2000
+intents = discord.Intents(messages=True, guilds=True, members=True, bans=True, emojis=True, reactions=True, typing=True, presences=True)
+
 client = commands.Bot(
     command_prefix = current_settings["settings"]["prefix"], 
     owner_id = current_settings["settings"]["owner"],
     help_command=discord.ext.commands.DefaultHelpCommand(dm_help=True),
-    allowed_mentions=discord.AllowedMentions(everyone=False, users=True, roles=True))
+    allowed_mentions=discord.AllowedMentions(everyone=False, users=True, roles=True),
+    intents=intents)
 
 
 #region ---------------- Event Listeners ----------------
@@ -38,8 +46,8 @@ async def on_command_error(ctx, error):
         return
 
     to_delete = ctx.message    
-    await ctx.send(content=f'{ctx.message.author.mention}: **ERROR:** ``{error}``', delete_after=6.0)
-    await to_delete.delete(delay=5)
+    await ctx.reply(content=f'**ERROR:** `{error}`', delete_after=6.0)
+    await to_delete.delete(delay=7)
 
     if type(error) == discord.ext.commands.errors.CommandOnCooldown or type(error) == discord.ext.commands.errors.MissingRole:
         return
@@ -48,6 +56,26 @@ async def on_command_error(ctx, error):
 async def on_command_completion(ctx):
     # Log Command evocations to the console at INFO level
     logger.LogPrint(f'Executing command {ctx.command.name} for user {ctx.message.author}', logging.INFO)
+
+@client.event
+async def on_message(ctx):
+    global hitroll
+    if ctx.guild.id == 107847342006226944 and ctx.author.bot == False and not ctx.content.startswith(current_settings["settings"]["prefix"]):
+        if 'chatbot' in ctx.content.lower() or random.randint(0,hitroll) == hitroll:
+            await ctx.channel.trigger_typing()            
+            if len(ctx.content) > 0:
+                truemessage = ctx.content
+                if truemessage.lower().startswith('chatbot'):
+                    truemessage = truemessage.replace("chatbot", " ")
+                    truemessage = truemessage.replace("Chatbot", " ")
+                response = convo.say(truemessage)
+                hitroll = 2000
+                await ctx.reply(content=f'`{response}`', mention_author=False)
+    hitroll -= 1
+    if hitroll <1:
+        hitroll = 1
+    await client.process_commands(ctx)
+    
 
 #@client.event
 #async def on_error(ctx, error):
