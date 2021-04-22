f18156c5e66d7b81a385a9482a7c690811dcacbb
NeighbourJim <c.proudmore@gmail.com>
2021-04-22 20:40:46 +0800

    Added a check for multiple links in !createtag to reduce spam

--- cogs/tags.py
+++ cogs/tags.py
@@ -1,5 +1,6 @@
 import discord
 import logging
+import re
 from discord.ext import commands
 from discord.ext.commands import BucketType
 from os import path
@@ -37,15 +38,13 @@ class Tags(commands.Cog):
         try:
             self.CheckAndCreateDatabase(ctx)
 
-            to_delete = ctx.message
             where = ("tag_name", Helpers.CommandStrip(self, ctx.message.content))
             tag = dbm.Retrieve(f"tags{ctx.guild.id}", "tags", [where])
             if len(tag) > 0:
-                await ctx.send(f'{ctx.message.author.mention}: Tag - {Helpers.CommandStrip(self, ctx.message.content)}\n{tag[0][1]}')
+                await ctx.reply(f'{tag[0][1]}')
             else:
                 ctx.command.reset_cooldown(ctx)
-                await ctx.send(f'{ctx.message.author.mention}: No tag with that name.', delete_after=6)
-            await to_delete.delete(delay=3)
+                await ctx.reply(f'No tag with that name.', delete_after=6)
         except Exception as ex:
             logger.LogPrint(f'ERROR - Couldn\'t execute tag command: {ex}',logging.ERROR)
 
@@ -56,24 +55,27 @@ class Tags(commands.Cog):
     async def createtag(self, ctx):
         try:
             self.CheckAndCreateDatabase(ctx)
-
-            to_delete = ctx.message
             split_message = Helpers.CommandStrip(self, ctx.message.content).split(' ')
             if len(split_message) > 1 and len(split_message) < 400:
                 tag_name = split_message[0]
                 tag_content = ' '.join(split_message[1:])
+
+                links = re.findall("(?P<url>https?://[^\s]+)", tag_content)
+                if len(links) > 1:
+                    await ctx.reply(f'No more than one link in a tag.')
+                    return
+
                 where = ("tag_name", tag_name)
                 existing = dbm.Retrieve(f"tags{ctx.guild.id}", "tags", [where])
                 if len(existing) == 0:
                     new_tag = {"tag_name": tag_name, "tag_content": tag_content, "author_id": ctx.message.author.id}
                     if dbm.Insert(f"tags{ctx.guild.id}", "tags", new_tag):
-                        await ctx.send(f'{ctx.message.author.mention}: Created tag \'{tag_name}\'.')
+                        await ctx.reply(f'Created tag \'{tag_name}\'.')
                 else:
                     ctx.command.reset_cooldown(ctx)
-                    await ctx.send(f'{ctx.message.author.mention}: That tag already exists.', delete_after=5)
+                    await ctx.reply(f'That tag already exists.', delete_after=5)
             else:
-                await ctx.send(f'{ctx.message.author.mention}: Tag too long or too short.')
-            await to_delete.delete(delay=5)
+                await ctx.reply(f'Tag too long or too short.')
         except Exception as ex:
             logger.LogPrint(f'ERROR - Couldn\'t create tag: {ex}',logging.ERROR)
 
@@ -85,7 +87,6 @@ class Tags(commands.Cog):
         try:
             self.CheckAndCreateDatabase(ctx)
 
-            to_delete = ctx.message
             tag_name = Helpers.CommandStrip(self, ctx.message.content)
             where = ("tag_name", tag_name)
             where_dict = {"tag_name": tag_name}
@@ -94,14 +95,13 @@ class Tags(commands.Cog):
                 user_is_admin = ctx.message.author.permissions_in(ctx.message.channel).administrator
                 if user_is_admin or ctx.message.author.id == existing[0][2]:
                     if dbm.Delete(f"tags{ctx.guild.id}", "tags", where_dict) > 0:
-                        await ctx.send(f'{ctx.message.author.mention}: Tag \'{tag_name}\' deleted.')
+                        await ctx.reply(f'Tag \'{tag_name}\' deleted.')
                 else:
                     ctx.command.reset_cooldown(ctx)
-                    await ctx.send(f'{ctx.message.author.mention}: You do not have permissions to delete that tag.\nYou must either be an Administrator or the original tag creator.', delete_after=5)
+                    await ctx.reply(f'You do not have permissions to delete that tag.\nYou must either be an Administrator or the original tag creator.', delete_after=5)
             else:
                 ctx.command.reset_cooldown(ctx)
-                await ctx.send(f'{ctx.message.author.mention}: That tag does not exist.', delete_after=5)
-            await to_delete.delete(delay=5)
+                await ctx.reply(f'That tag does not exist.', delete_after=5)
         except Exception as ex:
             logger.LogPrint(f'ERROR - Couldn\'t delete tag: {ex}',logging.ERROR)
 
@@ -123,10 +123,10 @@ class Tags(commands.Cog):
                     for item in tag_details_list:
                         list_file.write('%s\n\n' % item)
                 with open(f'{self.db_folder}taglist-{ctx.guild.id}.txt', 'rb') as fp:
-                    await ctx.send(f'{ctx.message.author.mention}', file=discord.File(fp, f'taglist-{ctx.guild.id}.txt'))
+                    await ctx.reply(f'{ctx.message.author.mention}', file=discord.File(fp, f'taglist-{ctx.guild.id}.txt'))
             else:
                 ctx.command.reset_cooldown(ctx)
-                await ctx.send(f'{ctx.message.author.mention}: No tags in the database.', delete_after=5)
+                await ctx.reply(f'No tags in the database.', delete_after=5)
         except Exception as ex:
             logger.LogPrint(f'ERROR - Couldn\'t get tag list: {ex}',logging.ERROR)
 
